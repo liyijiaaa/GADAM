e87bfd41fe871cd0c5d76aecd9f7cfdd68a7edf4
liyijiaaa <2979038380@qq.com>
2025-07-08 15:45:55 +0800

    修改的正态GADAM

--- model.py
+++ model.py
@@ -121,9 +121,9 @@ class GlobalModel(nn.Module):
         self.g = graph
         self.discriminator = Discriminator(out_dim)
         # self.beta = 0.9
-        self.bata1=0.4
-        self.bata2=0.4
-        self.bata3=0.2
+        self.bata1=0.2
+        self.bata2=0.7
+        self.bata3=0.1
         self.neigh_weight = 1. 
         self.loss = nn.BCEWithLogitsLoss()
         self.nor_idx = nor_idx
