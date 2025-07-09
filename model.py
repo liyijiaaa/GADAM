0b0ae20ba84b44aa29dee08095e86a98fe2ed0d1
liyijiaaa <2979038380@qq.com>
2025-07-09 15:48:05 +0800

    修改的正态GADAM

--- model.py
+++ model.py
@@ -129,7 +129,7 @@ class GlobalModel(nn.Module):
         self.center = center # high confidence normal center
 
         self.encoder = Encoder(graph, in_dim, out_dim, activation)
-        self.pre_attn = self.pre_attention()
+
 
 
     def pre_attention(self, gcd):
