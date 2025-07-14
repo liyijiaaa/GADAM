f7bc88b6671c9743f3e035658d0f3a222e5144dd
liyijiaaa <2979038380@qq.com>
2025-07-14 16:09:23 +0800

    更新节点嵌入

--- run.py
+++ run.py
@@ -300,6 +300,10 @@ def main(args):
     in_feats = feats.shape[1]
 
     #在这里修改
+    device = args.gpu
+    if device >= 0:
+        torch.cuda.set_device(device)
+        feats = feats.cuda()
     graph = update_graph(graph, feats)
     #初始化局部分数网络模型
     local_net = LocalModel(graph,
